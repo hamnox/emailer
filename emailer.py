5e2b950fd6308d09c8f4dc05fc7503dc13d29a15
Melanie Heisey <mheisey.nox@gmail.com>
2016-03-21 08:12:06 -0700

    init with smtplib notes

---
+++ emailer.py
@@ -0,0 +1,59 @@
+#!/usr/bin/python
+
+import smtplib
+from email.MIMEMultipart import MIMEMultipart
+from email.MIMEText import MIMEText
+
+
+# see also: https://github.com/sendgrid/sendgrid-python
+# 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
+
+ 
+# http://naelshiab.com/tutorial-send-email-python/
+
+fromaddr = "mheisey.nox@gmail.com"
+toaddr = "THE EMAIL ADDRESS TO SEND TO" # TODO: Swapping out
+
+
+
+
+server = smtplib.SMTP('smtp.gmail.com', 587)
+server.starttls()
+server.login(fromaddr, "0bs1d14n4 n0x")
+ 
+text = email_generator(fromaddr, toaddr)
+
+server.sendmail("mheisey.nox@gmail.com", toaddr, msg)
+server.quit()
+
+
+
+
+def email_generator(from_email, to_email):
+    msg = MIMEMultipart()
+    msg['From'] = from_email
+    msg['To'] = to_email
+    msg['Subject'] = "SUBJECT OF THE MAIL"
+    body = "YOUR MESSAGE HERE"
+    msg.attach(MIMEText(body, 'plain'))
+    return msg.as_string()
+
+# components:
+    # format dates to readable
+    # pair up candidate email addresses
+        # hard constraint checker
+        # importing/storing constraint information
+        # optional: soft constraint checker
+    # generate next dates from constraints
+    # make batch of emails
+    # send emails loop
+
+    # test dates
+        # time zones
+        # do they match
+    # test emails
+        # formats correctly
+        # pairs correspond
+    # test sender
+        # server responds
+        # emails send
