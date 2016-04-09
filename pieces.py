f583a00478125c05758b03ef81b564b6175bef0f
Melanie Heisey <mheisey.nox@gmail.com>
2016-04-09 14:16:09 -0700

    input some people info

--- pieces.py
+++ pieces.py
@@ -1,5 +1,9 @@
 from datetime import datetime, timedelta
 from pytz import timezone
+
+import sendgrid
+import json
+
 # http://pythonhosted.org/pytz/
 # list of timezones codes is available here: http://stackoverflow.com/questions/13866926/python-pytz-list-of-timezones
 
@@ -51,6 +55,8 @@ class Person:
     def to_dict(self):
 #        http://www.blog.pythonlibrary.org/2013/01/11/how-to-get-a-list-of-class-attributes/
         return self.__dict__()
+    def _serialize_emails(self):
+        return json.dumps(self.email)
     # stub
     def make_availability(self, spans):
         """ takes a list of span times as [((day, hour), (day, hour)),..] and returns an Availability
@@ -62,9 +68,59 @@ class Person:
             newAvail.add_span(t1, t2)
         return newAvail
 
+def person_from_json(data_object):
+    """given a JSON object approximating a person, return a person object!"""
+    lePerson = Person()
+    # dunno if python json object follows same rules as javascript dictionaries
+    lePerson.name = data_object['name'] # want to gen random numbers
+    lePerson.email = data_object['email'] # want to make a random number
+    import re 
+    spans = re.findall("\[(\[(\d+), (\d+)\])*?\d\]", data_object['availability'])
+                # %i - %i" % (x.total_seconds()/3600, y.total_seconds()/3600) for x, y in self.times].__str__()
+    lePerson.availability = lePerson.make_availability(spans)
+    # lePerson.active = data_object['active']
+    lePerson.active = True
+    lePerson.contact_preferences = data_object['contact_preferences']
+    lePerson.skype = data_object['skype']
+    lePerson.hangouts = data_object['hangouts']
+    lePerson.phone = data_object['phone']
+    lePerson.mobile = data_object['mobile']
+    lePerson.location = data_object['location']
+    lePerson.timezone = timezone(data_object['timezone'])
+    lePerson.other = data_object['timezone']
+    lePerson.blurb = data_object['blurb']
+    lePerson.metadebug = data_object['metadebug']
+    lePerson.notes = data_object['notes']
+    return lePerson
 
 
 
+# PLANNING:
+# Okay... so each person has a timezone.
+# We need an object that's something like (p1, p2, Datetime, Contact Method)
+# alternatively a dict: {partners: [p1,p2] , datetime: Datetime,
+#       contact_type: [options]}... may not get to contact type this round
+    # at some point we need an option to get it by matching up p1/p2
+    # availability and picking a random time they both can go
+# for each email, we need to get the Readable date for that user,
+    # the GoogleCalendar Date, possibly a simple date in the partner's time
+    # zone, and possibly some alternate times (given in both TZs) to
+    # reschedule to....
+
+    # for inputting, we want an interface to grep people
+    # i think we need dynamic python. exec(file) works in python 2.x
+    # http://stackoverflow.com/questions/4624416/is-there-a-posibility-to-execute-a-python-script-while-being-in-interactive-mode
+
+# alternatively I want a command line loop to print options,
+    # grab people by name, and input <week #> <DayAbbr> <24hr time> in PT
+    # this will save as an offset off of pacific time
+
+
+
+
+# for inputting, need a way to print the names of people objects in a list or dict
+# DONE: for inputting, need a function to interpret
+# for inputting, want a way to save pairings and their offsets to a file
 
 
 class Availability:
@@ -138,7 +194,7 @@ class Availability:
                     self.times.pop(span)
             else:
                 index += 1
-        # eh really need to test this
+        # TODO: need to test this
 
 
     def test_values(self, t1,t2):
@@ -147,7 +203,19 @@ class Availability:
         assert t1 <= Availability.available_span
         assert t2 >= 0
         assert t2 <= Availability.available_span
-    
+
+weeknums = {"sun":0, "mon":1, "tue":2, "wed":3, "thu":4, "fri":5, "sat":6}
+def parse_offset_input(string):
+    # for inputting, need to turn <week #> <DayAbbr no period> <24hr time> in PT into an offset
+    # later on want a way to specify time zone for natural time range inputs
+        # maybe then I'll save in UTC and shift the offsets during Daylight savings
+    import re
+    results = re.findall("(\d) (sun|mon|tue|wed|thu|fri|sat)\.? (\d+):(\d+)", string.lower())
+    assert len(results) == 1
+    week = int(results[0][0]) - 1
+    day = weeknums[results[0][1]]
+    hour = int(results[0][2]) + float(results[0][3])/60.0
+    return (week * 7 + day) * 24 + hour
 
 
 __SAVEFILENAME = "base_emailer_data.json"
@@ -156,12 +224,23 @@ class MyEncoder(json.JSONEncoder):
     def default(self, o):
         if isinstance(o,Availability):
             return o._serialize()
+        if isinstance(o,Person):
+            temp = o.__dict__
+            temp['availability'] = temp['availability']._serialize()
+            temp['timezone'] =  temp['timezone'].zone
+            return json.JSONEncoder.default(self, temp)
         else:
-            return JSONEncoder.default(self, o)
+            return json.JSONEncoder.default(self, o)
+    def encode(self, o):
+        if isinstance(o, list) and isinstance(o.__iter__().next(), Person):
+            temp = {}
+            for p in o:
+                temp[p.email] = {"name": p.name, "timezone": p.timezone.zone}
+            return json.dumps(temp, indent=1)
 
 
-def save_to_file(data):
-    with open(__SAVEFILENAME, 'w') as outfile:
+def save_to_file(data, savefilename=__SAVEFILENAME):
+    with open(savefilename, 'w') as outfile:
         json.dump(data, outfile, cls=MyEncoder, indent=1)
 
 
@@ -260,21 +339,23 @@ def pair_people(people, constraints):
         pass
 
 
-def sendemail(recipient, message):
-    """take a recipient and message, email the message. return a tuple of http status code and ? message"""
-# stub
-    return ("200", message)
-
-    import sendgrid
-    sg = sendgrid.SendGridClient(Sendgrid_API_key)
+def make_message(subject, html, text):
+    """given strings with the subject, html content, and body text, give me
+     a message object back"""
     message = sendgrid.Mail()
-    message.add_to('John Doe <john@email.com>')
-    message.set_subject('Example')
-    message.set_html('Body')
-    message.set_text('Body')
-    message.set_from('Doe John <doe@email.com>')
+    message.set_subject(subject)
+    message.set_html(html)
+    message.set_text(text)
+    message.set_from('Mel Heisey <mheisey.nox@gmail.com>')
+    return message
+
+def sendemail(recipient, message):
+    """take a recipient email address and message, email the message.
+    return a tuple of http status code and ? message"""
+    sg = sendgrid.SendGridClient(Sendgrid_API_key)
+    message.add_to(recipient)
     status, msg = sg.send(message)
-    return (status, message)
+    return (status, msg)
 
 
 
@@ -293,11 +374,45 @@ def sendemail(recipient, message):
 #                    TESTS
 # ----------------------------------------------
 
+def test_parse_offset_input():
+    assert parse_offset_input("2 Sun. 10:00") == 178
+    assert parse_offset_input("2 wed 18:00") == 258
+    assert parse_offset_input("2 sun 11:00") == 179
+    assert parse_offset_input("1 Thu 11:00") == 107
+    assert parse_offset_input("2 wed. 13:30") == 253.5
+
+def test_fromJSON():
+    leobjects = []
+    with open('old_user_data.json', 'r') as datafile:
+        leobjects = json.load(datafile)
+
+    people = []
+    for leobject in leobjects:
+        people.append(person_from_json(leobject))
+    assert len(json.dumps(people, cls=MyEncoder, indent=1)) > 5
+
+
+def test_send_email():
+    #test_msg = make_message("Testy", "<font color=\"purple\">testing html</font>", "testing text")
+    templ = ""
+    with open("orig_gmail_template.html", "r") as template_file:
+        templ = "".join(template_file.readlines())
+        templ = templ.replace("\n","")
+        print templ
+    test_msg = make_message("Testy Template", templ, "")
+    status, msg = sendemail("mheisey.nox@gmail.com", test_msg)
+    assert status == 200
+
+
 def test():
+    test_parse_offset_input()
+    # test_send_email()
+    test_fromJSON()
     assert Sendgrid_API_key != None
     person_1 = Person("paul", "werd@gmail.com")
-#     _person_2 = Person("me", "me@me.org")
-#     __test_pairing = pair_people([_person_1, _person_2], lambda x: True)
+    _person_2 = Person("me", "me@me.org")
+    test_parse_offset_input()
+#    __test_pairing = pair_people([_person_1, _person_2], lambda x: True)
 
     # these only pass by coincidence right now
     # assert len(__test_pairing) == 1
@@ -318,10 +433,8 @@ def test():
 
     _availability_3 = person_1.make_availability([((3,2), (4,1)),((1,0),(2,0))])
     assert len(_availability_3.times) == 2
-    # mispelled var name
     assert _availability_3.times[0] == [timedelta(days=3, hours=2), timedelta(days=4, hours=1)]
     assert _availability_3.times[1] == [timedelta(days=1), timedelta(days=2)]
-
 
 
 
@@ -333,5 +446,5 @@ if __name__ == "__main__":
     print "testing..."
     test()
     print "passed!"
-    get_me()
+    # get_me()
 
